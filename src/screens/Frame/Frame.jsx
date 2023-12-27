0bb61de479ce30a6c819111a2ab50443c40b56de
moodercoder <2017n1191@gmail.com>
2023-12-28 02:54:55 +0500

    initial

---
+++ src/screens/Frame/Frame.jsx
@@ -0,0 +1,178 @@
+import React from "react";
+import "./style.css";
+
+export const Frame = () => {
+  return (
+    <div className="frame">
+      <div className="overlap-group">
+        <div className="rectangle" />
+        <div className="div">
+          <div className="div-wrapper">
+            <div className="text-wrapper">8:00</div>
+          </div>
+          <div className="div-wrapper">
+            <div className="text-wrapper">9:00</div>
+          </div>
+          <div className="div-wrapper">
+            <div className="text-wrapper">10:00</div>
+          </div>
+          <div className="div-wrapper">
+            <div className="text-wrapper">11:00</div>
+          </div>
+          <div className="div-wrapper">
+            <div className="text-wrapper">12:00</div>
+          </div>
+          <div className="div-wrapper">
+            <div className="text-wrapper">13:00</div>
+          </div>
+          <div className="div-wrapper">
+            <div className="text-wrapper">14:00</div>
+          </div>
+          <div className="div-wrapper">
+            <div className="text-wrapper">15:00</div>
+          </div>
+          <div className="div-wrapper">
+            <div className="text-wrapper">16:00</div>
+          </div>
+          <div className="div-wrapper">
+            <div className="text-wrapper">17:00</div>
+          </div>
+          <div className="div-wrapper">
+            <div className="text-wrapper">18:00</div>
+          </div>
+          <div className="div-wrapper">
+            <div className="text-wrapper">19:00</div>
+          </div>
+          <div className="div-wrapper">
+            <div className="text-wrapper">20:00</div>
+          </div>
+          <div className="div-wrapper">
+            <div className="text-wrapper">21:00</div>
+          </div>
+          <div className="div-wrapper">
+            <div className="text-wrapper">22:00</div>
+          </div>
+          <div className="div-wrapper">
+            <div className="text-wrapper">23:00</div>
+          </div>
+          <div className="div-wrapper">
+            <div className="text-wrapper">24:00</div>
+          </div>
+        </div>
+        <div className="div-2">
+          <div className="div-wrapper-2">
+            <div className="text-wrapper-2">Court 1</div>
+          </div>
+          <div className="div-wrapper-2">
+            <div className="text-wrapper-2">Court 2</div>
+          </div>
+          <div className="div-wrapper-2">
+            <div className="text-wrapper-2">Court 3</div>
+          </div>
+          <div className="div-wrapper-2">
+            <div className="text-wrapper-2">Court 4</div>
+          </div>
+          <div className="div-wrapper-2">
+            <div className="text-wrapper-2">Court 5</div>
+          </div>
+          <div className="div-wrapper-2">
+            <div className="text-wrapper-2">Court 6</div>
+          </div>
+          <div className="div-wrapper-2">
+            <div className="text-wrapper-2">Court 7</div>
+          </div>
+          <div className="div-wrapper-2">
+            <div className="text-wrapper-2">Court 8</div>
+          </div>
+          <div className="div-wrapper-2">
+            <div className="text-wrapper-2">Court 9</div>
+          </div>
+          <div className="div-wrapper-2">
+            <div className="text-wrapper-2">Court 10</div>
+          </div>
+          <div className="div-wrapper-2">
+            <div className="text-wrapper-2">Court 11</div>
+          </div>
+          <div className="div-wrapper-2">
+            <div className="text-wrapper-2">Court 12</div>
+          </div>
+        </div>
+        <div className="div-3">
+          <img className="line" alt="Line" src="/img/line-40.svg" />
+          <img className="img" alt="Line" src="/img/line-33.svg" />
+          <img className="img" alt="Line" src="/img/line-34.svg" />
+          <img className="img" alt="Line" src="/img/line-35.svg" />
+          <img className="img" alt="Line" src="/img/line-36.svg" />
+          <img className="img" alt="Line" src="/img/line-37.svg" />
+          <img className="img" alt="Line" src="/img/line-38.svg" />
+          <img className="img" alt="Line" src="/img/line-39.svg" />
+          <img className="img" alt="Line" src="/img/line-40.svg" />
+          <img className="img" alt="Line" src="/img/line-33.svg" />
+          <img className="img" alt="Line" src="/img/line-34.svg" />
+          <img className="img" alt="Line" src="/img/line-35.svg" />
+          <img className="img" alt="Line" src="/img/line-36.svg" />
+          <img className="img" alt="Line" src="/img/line-37.svg" />
+          <img className="img" alt="Line" src="/img/line-38.svg" />
+          <img className="img" alt="Line" src="/img/line-39.svg" />
+          <img className="img" alt="Line" src="/img/line-40.svg" />
+          <img className="img" alt="Line" src="/img/line-33.svg" />
+          <img className="img" alt="Line" src="/img/line-34.svg" />
+          <img className="img" alt="Line" src="/img/line-35.svg" />
+          <img className="img" alt="Line" src="/img/line-36.svg" />
+          <img className="img" alt="Line" src="/img/line-37.svg" />
+          <img className="img" alt="Line" src="/img/line-38.svg" />
+          <img className="img" alt="Line" src="/img/line-39.svg" />
+          <img className="img" alt="Line" src="/img/line-40.svg" />
+          <img className="img" alt="Line" src="/img/line-33.svg" />
+          <img className="img" alt="Line" src="/img/line-34.svg" />
+          <img className="img" alt="Line" src="/img/line-35.svg" />
+          <img className="img" alt="Line" src="/img/line-36.svg" />
+          <img className="img" alt="Line" src="/img/line-37.svg" />
+          <img className="img" alt="Line" src="/img/line-38.svg" />
+          <img className="img" alt="Line" src="/img/line-39.svg" />
+          <img className="img" alt="Line" src="/img/line-40.svg" />
+        </div>
+        <div className="div-4">
+          <img className="line-2" alt="Line" src="/img/line-44.svg" />
+          <img className="line-3" alt="Line" src="/img/line-44.svg" />
+          <img className="line-3" alt="Line" src="/img/line-45.svg" />
+          <img className="line-3" alt="Line" src="/img/line-46.svg" />
+          <img className="line-3" alt="Line" src="/img/line-48.svg" />
+          <img className="line-3" alt="Line" src="/img/line-48.svg" />
+          <img className="line-3" alt="Line" src="/img/line-49.svg" />
+          <img className="line-3" alt="Line" src="/img/line-50.svg" />
+          <img className="line-3" alt="Line" src="/img/line-53.svg" />
+          <img className="line-3" alt="Line" src="/img/line-53.svg" />
+          <img className="line-3" alt="Line" src="/img/line-53.svg" />
+        </div>
+        <img className="line-4" alt="Line" src="/img/line-73.svg" />
+        <div className="div-5">
+          <div className="div-wrapper-3">
+            <div className="text-wrapper-3">8:00 - 9:00</div>
+          </div>
+          <div className="div-6">
+            <div className="text-wrapper-4">Bilal</div>
+            <div className="div-wrapper-4">
+              <div className="text-wrapper-5">2/4</div>
+            </div>
+          </div>
+        </div>
+        <div className="div-7">
+          <div className="div-wrapper-3">
+            <div className="text-wrapper-3">9:00 - 10:00</div>
+          </div>
+          <div className="div-6">
+            <div className="text-wrapper-4">Bilal</div>
+            <div className="div-wrapper-5">
+              <div className="text-wrapper-6">1/4</div>
+            </div>
+          </div>
+        </div>
+        <img className="img-2" alt="Frame" src="/img/frame-427319089.svg" />
+        <div className="text-wrapper-7">10:47</div>
+      </div>
+      <div className="rectangle-2" />
+      <img className="rectangle-3" alt="Rectangle" src="/img/rectangle-2377.png" />
+    </div>
+  );
+};
